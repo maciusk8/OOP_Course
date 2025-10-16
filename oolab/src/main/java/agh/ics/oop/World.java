15b88f33f4507c7092b8721e4cbef1dd65e95ef2
maciusk8 <mikolajek.maciek@gmail.com>
2025-10-16 09:24:09 +0200

    finish tasks 10-16

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,16 +1,30 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MoveDirection;
+
 public class World
 {
-     static void main()
-     {
-            IO.println(String.format("System started."));
-            IO.println(String.format("System finished."));
-     }
+    public static final String petName = "Big Boss";
+    public static void main(String[] args)
+    {
+        var moveDriection = new MoveDirection[]{};
+        IO.println(String.format("System started."));
+        run(OptionParser.parseMoveDirections(args));
+        IO.println(String.format("System finished."));
+    }
 
-     private static void run()
+     private static void run(MoveDirection[] directions)
      {
-         IO.println(String.format("Big Boss is moving forward."));
+         for (var direction : directions)
+         {
+             switch (direction)
+             {
+                 case FORWARD -> IO.println(String.format("%s is moving forward.", petName));
+                 case BACKWARD -> IO.println(String.format("%s is moving backward.", petName));
+                 case LEFT -> IO.println(String.format("%s is turning left.", petName));
+                 case RIGHT -> IO.println(String.format("%s is turning right.", petName));
+             }
+         }
      }
 
 }
