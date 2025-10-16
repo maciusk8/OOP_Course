3e816c88bd4144b3642ae9112ad926a41a64f514
maciusk8 <mikolajek.maciek@gmail.com>
2025-10-16 08:27:08 +0200

    finish tasks 1-10

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,16 @@
+package agh.ics.oop;
+
+public class World
+{
+     static void main()
+     {
+            IO.println(String.format("System started."));
+            IO.println(String.format("System finished."));
+     }
+
+     private static void run()
+     {
+         IO.println(String.format("Big Boss is moving forward."));
+     }
+
+}
