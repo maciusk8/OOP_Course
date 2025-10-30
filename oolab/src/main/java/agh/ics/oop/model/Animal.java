a287ec197a69eb74faf88d364b15a3f8ea280520
maciusk8 <mikolajek.maciek@gmail.com>
2025-10-30 11:35:00 +0100

    add move method fix world

--- oolab/src/main/java/agh/ics/oop/model/Animal.java
+++ oolab/src/main/java/agh/ics/oop/model/Animal.java
@@ -4,21 +4,28 @@ import agh.ics.oop.MapDirection;
 import agh.ics.oop.Vector2d;
 
 import java.util.Objects;
-
-import static agh.ics.oop.World.petName;
-
 public class Animal
 {
     private static final MapDirection DEFAULT_ORIENTATION = MapDirection.NORTH;
     private MapDirection orientation;
     private Vector2d position;
-    Animal(Vector2d position) {orientation = DEFAULT_ORIENTATION; this.position = position;}
-    Animal() { this(new Vector2d(2,2)); }
-    boolean isAt(Vector2d position) {return Objects.equals(position, this.position);}
+    public Animal(Vector2d position) {orientation = DEFAULT_ORIENTATION; this.position = position;}
+    public Animal() { this(new Vector2d(2,2)); }
+    public boolean isAt(Vector2d position) {return Objects.equals(position, this.position);}
+    public void move(MoveDirection direction)
+    {
+        switch (direction)
+        {
+            case RIGHT -> orientation = orientation.next();
+            case LEFT -> orientation = orientation.previous();
+            case FORWARD -> position = position.add(orientation.toUnitVector());
+            case BACKWARD -> position = position.subtract(orientation.toUnitVector());
+        }
+    }
 
     @Override
     public String toString()
     {
-        return String.format("Animal at %s facing %s", position, orientation);
+        return String.format("Zwierzak na pozycji %s, patrzy na %s", position, orientation);
     }
 }
