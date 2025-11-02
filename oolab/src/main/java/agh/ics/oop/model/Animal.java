76574384f468cf37df6da8ecf7bbc1383ddd39b0
maciusk8 <mikolajek.maciek@gmail.com>
2025-11-02 16:04:49 +0100

    add animalTest + change Animal accordingly + change const logic + minor fixes

--- oolab/src/main/java/agh/ics/oop/model/Animal.java
+++ oolab/src/main/java/agh/ics/oop/model/Animal.java
@@ -5,18 +5,28 @@ import agh.ics.oop.Vector2d;
 
 import java.util.Objects;
 
-import static agh.ics.oop.World.LOWER_LEFT_CORNER;
-import static agh.ics.oop.World.UPPER_RIGHT_CORNER;
+import static agh.ics.oop.World.*;
 
 public class Animal
 {
-    private static final MapDirection DEFAULT_ORIENTATION = MapDirection.NORTH;
+    public static final MapDirection DEFAULT_ORIENTATION = MapDirection.NORTH;//Uwazam, ze na potrzeby testow te stałe powinny być publiczne.
+    public static final Vector2d DEFAULT_POSITION = CENTER;
+    private final Vector2d lowerLeftCorner;
+    private final Vector2d upperRightCorner;
+
     private MapDirection orientation;
     private Vector2d position;
-    public Animal(Vector2d position) {orientation = DEFAULT_ORIENTATION; this.position = position;}
-    public Animal() { this(new Vector2d(2,2)); }
+    public Animal(Vector2d position)
+    {
+        this.orientation = DEFAULT_ORIENTATION;
+        this.position = position;
+        this.lowerLeftCorner = LOWER_LEFT_CORNER;
+        this.upperRightCorner = UPPER_RIGHT_CORNER;
+    }
+    public Animal() { this(DEFAULT_POSITION); }
     public boolean isAt(Vector2d position) {return Objects.equals(position, this.position);}
-    public void move(MoveDirection direction) //Myśle, ze lepiej byloby zrobic dwie publiczne metody move i turn, ale robie tak jak jest w poleceniu. Nie rozbijam na dwie prywatne metody bo nie zwiększyłoby to znacznie czytelności a napewno więcej linijek by to zajelo.
+    public boolean isFacing(MapDirection orientation) {return this.orientation == orientation;}
+    public void move(MoveDirection direction)
     {
         Vector2d newPosition = null;
 
@@ -35,12 +45,22 @@ public class Animal
     }
     private boolean isWithinBounds(Vector2d position)
     {
-        return position.follows(LOWER_LEFT_CORNER) && position.precedes(UPPER_RIGHT_CORNER);
+        return position.follows(lowerLeftCorner) && position.precedes(upperRightCorner);
     }
 
     @Override
     public String toString()
     {
         return String.format("na pozycji %s, patrzy na %s", position, orientation);
+    }
+
+    //Konstruktor na potrzeby testu SimulationTest
+    public Animal(Vector2d position, MapDirection orientation, Vector2d lowerLeftCorner, Vector2d upperRightCorner) {
+        {
+            this.position = position;
+            this.orientation = orientation;
+            this.lowerLeftCorner = lowerLeftCorner;
+            this.upperRightCorner = upperRightCorner;
+        }
     }
 }
---
+++ oolab/src/test/java/agh/ics/oop/model/AnimalTest.java
@@ -0,0 +1,126 @@
+package agh.ics.oop.model;
+
+import agh.ics.oop.MapDirection;
+import agh.ics.oop.Vector2d;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static agh.ics.oop.World.*;
+import static agh.ics.oop.model.Animal.*;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalTest
+{ @ParameterizedTest()
+    @CsvSource({
+            "RIGHT, NORTH, EAST",
+            "LEFT, NORTH, WEST",
+            "RIGHT, SOUTH, WEST",
+            "LEFT, SOUTH, EAST",
+            "RIGHT, WEST, NORTH",
+            "LEFT, WEST, SOUTH",
+            "RIGHT, EAST, SOUTH",
+            "LEFT, EAST, NORTH",
+    })
+    void runMethodShouldChangeOrientationGivenLeftOrRight(MoveDirection direction, MapDirection lookingAt, MapDirection expected)
+    {
+        var animal = new Animal(DEFAULT_POSITION, lookingAt, LOWER_LEFT_CORNER, UPPER_RIGHT_CORNER); //Given
+        animal.move(direction); //When
+        assertTrue(animal.isFacing(expected)); //Then
+    }
+
+    @ParameterizedTest
+    @MethodSource("moveDataFirst")
+    void runMethodShouldChangePositionGivenForwardOrBackward(MoveDirection direction, MapDirection lookingAt, Vector2d expected)
+    {
+        var animal = new Animal();
+
+        animal.move(direction);
+
+        assertTrue(animal.isAt(expected));
+    }
+
+    private static Stream<Arguments> moveDataFirst()
+    {
+        return Stream.of(
+                Arguments.of(MoveDirection.FORWARD, MapDirection.NORTH, DEFAULT_POSITION.add(MapDirection.NORTH.toUnitVector())),
+                Arguments.of(MoveDirection.BACKWARD, MapDirection.NORTH, DEFAULT_POSITION.add(MapDirection.SOUTH.toUnitVector()))
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "RIGHT",
+            "LEFT"
+    })
+    void runMethodShouldNotChangePositionGivenLeftOrRight(MoveDirection direction)
+    {
+        var animal = new Animal();;
+
+        animal.move(direction);
+
+        assertTrue(animal.isAt(DEFAULT_POSITION));
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("moveDataSecond")
+    void runMethodShouldNotChangePositionBeingOnBorderLookingOutside(MapDirection lookingAt, Vector2d beingAt)
+    {
+        var animal = new Animal(beingAt, lookingAt, LOWER_LEFT_CORNER, UPPER_RIGHT_CORNER);
+
+        animal.move(MoveDirection.FORWARD);
+
+        assertTrue(animal.isAt(beingAt));
+    }
+
+    private static Stream<Arguments> moveDataSecond()
+    {
+        return Stream.of(
+                Arguments.of(MapDirection.WEST, LOWER_LEFT_CORNER),
+                Arguments.of(MapDirection.SOUTH, LOWER_LEFT_CORNER),
+                Arguments.of(MapDirection.NORTH, UPPER_RIGHT_CORNER),
+                Arguments.of(MapDirection.EAST, UPPER_RIGHT_CORNER)
+                );
+    }
+
+    @Test
+    void runMethodShouldNotChangePositionOutsideBorder()
+    {
+        var vectorOutsideBorder = UPPER_RIGHT_CORNER.add(new Vector2d(1,1));
+        var animal = new Animal(vectorOutsideBorder, DEFAULT_ORIENTATION, LOWER_LEFT_CORNER,  UPPER_RIGHT_CORNER);
+        animal.move(MoveDirection.FORWARD);
+        assertTrue(animal.isAt(vectorOutsideBorder));
+    }
+    @Test
+    void toStringFormatChek()
+    {
+        var animal = new Animal();
+        assertEquals(String.format("na pozycji %s, patrzy na %s", DEFAULT_POSITION, DEFAULT_ORIENTATION) , animal.toString());
+    }
+
+    @Test
+    void defaultOrientationCheck()
+    {
+        var animal = new Animal();
+        assertTrue(animal.isFacing(DEFAULT_ORIENTATION));
+    }
+    @Test
+    void defaultPositionCheck()
+    {
+        var animal = new Animal();
+        assertTrue(animal.isAt(DEFAULT_POSITION));
+    }
+
+    @Test
+    void secondConstructorCheck()
+    {
+        var animal = new Animal(CENTER);
+        assertTrue(animal.isAt(CENTER));
+        assertTrue(animal.isFacing(DEFAULT_ORIENTATION));
+    }
+}
